6f600cb463775fe7c8fb37198901f196232e5ccb
nairo-mudumane <nairomudumane@outlook.com>
2022-09-16 12:49:57 +0200

    updated speech create route

--- src/controllers/speech.js
+++ src/controllers/speech.js
@@ -3,9 +3,39 @@ const utils = require("../utils/speech");
 
 const create = async (req, res) => {
   const payload = req.body;
+  let pastSpeeches;
 
-  if (payload && payload.year && utils.isValidYear(payload)) {
-    return res.status(400).json({ message: "invalid year" });
+  if (!payload) {
+    return res.status(400).json({ message: "no data provided" });
+  } else if (payload.year) {
+    if (!utils.isValidYear(payload.year)) {
+      return res.status(400).json({ message: "invalid year" });
+    }
+  }
+
+  if (payload.year) {
+    try {
+      pastSpeeches = await model.find({ year: payload.year });
+      if (pastSpeeches && pastSpeeches.length > 0) {
+        throw new Error();
+      } else {
+        try {
+          payload["year"] = new Date().getFullYear();
+          const result = await model.create(payload);
+
+          return res
+            .status(201)
+            .json({ message: "created", data: { _id: result.id } });
+        } catch (error) {
+          console.error(error);
+          return res.status(400).json({ message: error.message });
+        }
+      }
+    } catch (error) {
+      return res.status(400).json({
+        message: `can not duplicate years. ${payload.year} already registered.`,
+      });
+    }
   }
 
   try {
@@ -28,14 +58,18 @@ const getByYear = async (req, res) => {
   }
 
   try {
-    const speech = model.find({ year: params.year }).sort({ createdAt: -1 });
+    const speech = await model
+      .find({ year: params.year })
+      .sort({ createdAt: -1 });
 
     if (!speech) {
       return res.status(404).json({ message: "speech not found" });
     }
 
     return res.status(200).json({ message: "ok", data: speech });
-  } catch (error) {}
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const getAll = async (req, res) => {
--- src/utils/speech.js
+++ src/utils/speech.js
@@ -1,17 +1,7 @@
-const regex = /^(202[0-2]|20[0-2]\d)$/;
-// |200\d|201[0-3]
-
 function isValidYear(year) {
-  if (year && year.length === 4) {
-    try {
-      const numYear = parseInt(year);
-      console.log("regex valueOf: ", regex.test(numYear).valueOf());
-      console.log("regex: ", regex.test(numYear));
-      if (regex.test(numYear)) {
-        return true;
-      }
-    } catch (error) {
-      return false;
+  if (year) {
+    if (year >= 2020 && year <= 2022) {
+      return true;
     }
   }
   return false;
