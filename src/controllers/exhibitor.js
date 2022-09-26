e8a5ca6000f13e9c1c143df96aba2d7e019e472e
nairo-mudumane <nairomudumane@outlook.com>
2022-09-26 18:02:23 +0200

    updated exhibitor's routes

--- src/controllers/exhibitor.js
+++ src/controllers/exhibitor.js
@@ -1,8 +1,9 @@
 const model = require("../models/exhibitor");
-const { isObjectEmpty } = require("../utils/empty");
+const { isObjectEmpty, isEmpty } = require("../utils/empty");
 const utils = require("../utils/exhibitor");
 const passwordUtils = require("../utils/password");
 const queryUtils = require("../utils/query");
+const dbUtils = require("../utils/mongoose");
 
 const create = async (req, res) => {
   const payload = req.body;
@@ -57,16 +58,50 @@ const getAll = async (req, res) => {
 
       const totalResults = filteredExhibitors.length;
 
+      filteredExhibitors.map((exhibitor) => {
+        const formatted = passwordUtils.removePasswordField(exhibitor._doc);
+        noPasswordExhibitors.push(formatted);
+      });
+
       return res
         .status(200)
-        .json({ message: "ok", totalResults, data: filteredExhibitors });
+        .json({ message: "ok", totalResults, data: noPasswordExhibitors });
     } catch (error) {
       return res.status(500).json({ message: error.message });
     }
   }
 };
 
+const getById = async (req, res) => {
+  const params = req.params;
+
+  if (isEmpty(params.id)) {
+    return res.status(400).json({ message: "parameter 'id' is required" });
+  }
+
+  if (!dbUtils.isValidObjectId(params.id)) {
+    return res.status(400).json({ message: "invalid objectId format" });
+  }
+
+  try {
+    const result = await model.findById(params.id);
+
+    if (isObjectEmpty(result)) {
+      return res.status(404).json({ message: "not found" });
+    }
+
+    const noPasswordExhibitor = passwordUtils.removePasswordField(result._doc);
+
+    return res
+      .status(200)
+      .json({ message: "ok", totalResults: 1, data: noPasswordExhibitor });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   create,
   getAll,
+  getById,
 };
--- src/routes/exhibitor.js
+++ src/routes/exhibitor.js
@@ -3,7 +3,7 @@ const controller = require("../controllers/exhibitor");
 
 const ExhibitorRoutes = (app) => {
   app.route("/exhibitors").post(controller.create).get(controller.getAll);
-  app.post("/dev/exhibitors", postRandomData);
+  app.route("/exhibitors/:id").get(controller.getById);
 };
 
 module.exports = {
